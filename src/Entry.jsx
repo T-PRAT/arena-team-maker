2e8401130ee87171424ac513e16033bb97f0a2ba
tprat <Titouan.p@hotmail.fr>
2023-12-14 14:28:20 +0100

    better style

--- src/Entry.jsx
+++
@@ -1,8 +0,0 @@
-export default function Entry() {
-	return (
-		<>
-			<label htmlFor="message" className="mb-2 text-sm">Your message</label>
-			<textarea id="message" rows="4" className="" placeholder="Your message..."></textarea>
-		</>
-	)
-}
